05a8ba8a6c5fe7d57759a54e61fc18632b2456b5
Dávid Czompó <czompi@czompi.hu>
2023-01-20 21:16:25 +0100

    Migrate to new hosting model

--- CWCT-MA/Program.cs
+++ CWCT-MA/Program.cs
@@ -1,75 +1,110 @@
-using CwctMa.Helpers;
 using CwctMa.Model;
-using CwctMa.Model.Xmd;
+using Markdig;
+using Markdig.Xmd;
+using Markdig.Prism;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using CwctMa.Extensions;
 using Serilog;
 using Serilog.Events;
 using Serilog.Sinks.SystemConsole.Themes;
-using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
-using System.Text.Json;
+using System;
 
-namespace CwctMa
+string CzSoftCdnCors = "_cscdnmacors";
+
+CzomPack.Settings.Application = new CzomPack.Application(typeof(Program).Assembly);
+CzomPack.Settings.WorkingDirectory = Globals.DataDirectory;
+
+Globals.LoadConfigs();
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .Enrich.FromLogContext()
+    .WriteTo.File(
+        Path.Combine(Globals.LogsDirectory, @$"{Assembly.GetExecutingAssembly().GetName().Name}.{DateTime.Now:yyyy-MM-dd}.log"),
+        outputTemplate: "[{Timestamp:HH:mm:ss}] [{Level}] [{SourceContext}] {Message}{NewLine}{Exception}",
+        fileSizeLimitBytes: 1_000_000,
+#if RELEASE
+        restrictedToMinimumLevel: LogEventLevel.Information,
+#else
+        restrictedToMinimumLevel: LogEventLevel.Verbose,
+#endif
+        rollOnFileSizeLimit: true,
+        shared: true,
+        flushToDiskInterval: TimeSpan.FromSeconds(1))
+    .WriteTo.Console(
+        outputTemplate: "[{Timestamp:HH:mm:ss}] [{Level}] [{SourceContext}] {Message}{NewLine}{Exception}",
+        theme: AnsiConsoleTheme.Literate,
+#if RELEASE
+        restrictedToMinimumLevel: LogEventLevel.Information
+#else
+        restrictedToMinimumLevel: LogEventLevel.Verbose
+#endif
+    )
+    .CreateLogger();
+
+try
 {
-    public class Program
-    {
-        public static void Main(string[] args)
+    Log.Information("Starting host...");
+    Log.Information(Globals.ConfigFile);
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog();
+
+    builder.Services.AddRazorPages();
+    builder.Services.AddServerSideBlazor();
+    builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+    builder.Services.AddHttpContextAccessor();
+    builder.Services.AddCors(options => {
+        options.AddPolicy(name: CzSoftCdnCors,
+        builder =>
         {
-            CzomPack.Settings.Application = new CzomPack.Application(typeof(Program).Assembly);
-            CzomPack.Settings.WorkingDirectory = Globals.DataDirectory;
+            builder.WithOrigins(
+                "https://cdn.czsoft.hu",
+                "https://cdn-beta.czsoft.hu",
+                "https://cdn.czompisoftware.hu",
+                "https://cdn-beta.czompisoftware.hu"
+            );
+        });
+    });
 
-            Globals.LoadConfigs();
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .Enrich.FromLogContext()
-                .WriteTo.File(
-                    Path.Combine(Globals.LogsDirectory, @$"{Assembly.GetExecutingAssembly().GetName().Name}.{DateTime.Now:yyyy-MM-dd}.log"),
-                    outputTemplate: "[{Timestamp:HH:mm:ss}] [{Level}] [{SourceContext}] {Message}{NewLine}{Exception}",
-                    fileSizeLimitBytes: 1_000_000,
-#if RELEASE
-                    restrictedToMinimumLevel: LogEventLevel.Information,
-#else
-                    restrictedToMinimumLevel: LogEventLevel.Verbose,
-#endif
-                    rollOnFileSizeLimit: true,
-                    shared: true,
-                    flushToDiskInterval: TimeSpan.FromSeconds(1))
-                .WriteTo.Console(
-                    outputTemplate: "[{Timestamp:HH:mm:ss}] [{Level}] [{SourceContext}] {Message}{NewLine}{Exception}",
-                    theme: AnsiConsoleTheme.Literate,
-#if RELEASE
-                    restrictedToMinimumLevel: LogEventLevel.Information
-#else
-                    restrictedToMinimumLevel: LogEventLevel.Verbose
-#endif
-                )
-                .CreateLogger();
-
-            try
-            {
-                Log.Information("Starting host...");
-                Log.Information(Globals.ConfigFile);
-                CreateHostBuilder(args).Build().Run();
-            }
-            catch (Exception ex)
-            {
-                Log.Fatal(ex, "Host terminated unexpectedly.");
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .UseSerilog()
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+    var app = builder.Build();
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
     }
+    else
+    {
+        app.UseExceptionHandler("/Error");
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+    app.UseCloudFlareConnectingIp();
+
+    app.UseRouting();
+    app.UseCors(CzSoftCdnCors);
+
+    Globals.MarkdownPipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().UsePrism().UseXMDLanguage().Build();
+
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapBlazorHub();
+        endpoints.MapFallbackToPage("/_Host");
+    });
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly.");
+}
+finally
+{
+    Log.CloseAndFlush();
 }
